0a1348b8ed08f67d2904d9850ea754c49b926f18
Mujahid Hussain <hmujahid08@gmail.com>
2024-12-19 23:24:29 +0500

    `Added ForgotPassword page and route to App.js`

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import BugDetails from "./pages/BugDetails";
 import CreateBug from "./pages/CreateBug";
 import EditBug from "./pages/EditBug";
+import ForgotPassword from "./pages/ForgotPassword";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
         <Route path="/bug-detail/:id" element={<BugDetails />} />
         <Route path="/create-bug" element={<CreateBug />} />
